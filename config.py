fe0a9a0d38f3147faaa3bbf9c50ba1a809d9b286
testhdm <83900038+testhdm@users.noreply.github.com>
2021-07-14 10:34:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-bot_token = "wtf" # yourbot token
-ARQ_API_KEY = "sexmmm" # Get this from @ARQBot
-api_id = 6 # your api id
-api_hash = "69696969696sexm" # your apihash
+bot_token = "1741302430:AAG1F-bSo9a_6D2QiPA24xuQVLfyKJHDSTE" # yourbot token
+ARQ_API_KEY = "XQYJAL-HTSZIK-YALWDS-TJPWMO-ARQ" # Get this from @ARQBot
+api_id = 6469237 # your api id
+api_hash = "aaaacf752e28564b49714c7ad51fd48b" # your apihash
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
 # Leave it as it is
