251baa0f7387dae3265e8e09f61019ce57673b5b
EliteDarkRay <71128663+EliteDarkRay@users.noreply.github.com>
2021-08-07 13:49:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ api_hash = "aaaacf752e28564b49714c7ad51fd48b" # your apihash
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
 # Leave it as it is
-LANGUAGE = "en"
+LANGUAGE = "ml"
 # Leave it as it is
 ARQ_API_BASE_URL = "https://thearq.tech"
